f6fc3dc082785f04b0e2e1c0d287a72a0e3ca1b5
pikoiv <pikoiv@ttu.ee>
2020-09-21 19:11:18 +0300

    added category component to git

---
+++ team31-frontend/src/app/views/category/category.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Category} from "../../category";
+import {CategoryService} from "../../category.service";
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+  categoryId;
+  category:Category;
+  defaultImage = 'https://longsshotokan.com/wp-content/uploads/2017/04/default-image-620x600.jpg';
+
+  constructor(private route: ActivatedRoute,
+              private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) =>  {
+      this.categoryId = params.get('id');
+      this.getCategory();
+    });
+  }
+
+  getCategory() {
+    this.categoryService.getCategoryById(this.categoryId).subscribe(
+      data => {
+        this.category = data;
+        console.log(data);
+      }
+    )
+  }
+
+}
