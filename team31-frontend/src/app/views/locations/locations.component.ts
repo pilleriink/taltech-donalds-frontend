94aaf7dc99e36e641ebac84f38465dc9f8306b15
sopham <shuong1012@gmail.com>
2020-09-22 09:25:52 +0300

    changing location

--- team31-frontend/src/app/views/locations/locations.component.ts
+++ team31-frontend/src/app/views/locations/locations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import  * as L from 'leaflet';
 import 'mapbox-gl-leaflet';
 
@@ -7,46 +7,19 @@ import 'mapbox-gl-leaflet';
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.css']
 })
-export class LocationsComponent implements OnInit, AfterViewInit {
+export class LocationsComponent implements AfterViewInit {
+  private map;
 
-  private map: L.Map;
+  constructor() {}
 
-  @ViewChild('map',{static: false})
-  private mapContainer: ElementRef<HTMLElement>;
-
-  constructor() { }
-
-  ngOnInit() {
-    var map = L.map('map', { center: [34, 8], zoom: 7 });
-    var url = 'http://localhost:8080/geoserver/webmapespace/wms';
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
   }
-
-  ngAfterViewInit() {
-    const myAPIKey = "731e1cda10e94dc2bb339db1047835ba";
-    const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
-    const initialState = {
-      lng: 11,
-      lat: 49,
-      zoom: 4
-    }
-    const map = new L.Map(this.mapContainer.nativeElement).setView(
-      [initialState.lat, initialState.lng],
-      initialState.zoom
-    );
-
-    map.attributionControl
-      .setPrefix("")
-      .addAttribution(
-        'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a ' +
-        'href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
-      );
-
-    L.mapboxGL({
-      style: `${mapStyle}?apikey=${myAPIKey}`,
-      accessToken: "no-token"
-    }).addTo(map);
-
+  ngAfterViewInit(): void {
+    this.initMap()
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
