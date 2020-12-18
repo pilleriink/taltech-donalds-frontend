8db6cdadd9aa5af7bb32c6c1814cc4fed2cd793c
Song Huong Pham Thi <sopham@ttu.ee>
2020-12-18 13:15:39 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ WORKDIR=/opt/taltech-donald-frontend/
 
 cd $WORKDIR
 
-/usr/bin/env npm run -- ng serve --host 0.0.0.0 --disable-host-check
+/usr/bin/env npm run -- ng build --prod --host 0.0.0.0 --disable-host-check
